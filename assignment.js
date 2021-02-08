40b782808ed1bb7417a7dfb993cc342b93bccfc4
Rezuan Ahmed <zikzack06@gmail.com>
2021-02-08 22:51:59 +0600

    Basic

---
+++ assignment.js
@@ -0,0 +1,16 @@
+// https://github.com/Rezuan-Ahmed-git/assignment.js.git
+
+// (1) kilometer To Meter start
+// (1) kilometer To Meter End
+
+
+// (2) Budget Calculator start
+// (2) Budget Calculator End
+
+
+// (3) Hotel Cost start
+// (3) Hotel Cost End
+
+
+// (4) Mega Friend start
+// (4) Mega Friend End
